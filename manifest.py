d84eff7f9167cb70eeb69d526682c2c3134222c1
Metallerie <98831730+Metallerie@users.noreply.github.com>
2024-12-28 12:28:45 +0100

    Create manifest.py

---
+++ manifest.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Product Image WebP Converter',
+    'version': '1.0',
+    'summary': 'Convert product images to WebP format upon upload',
+    'depends': ['product'],
+    'data': [],
+    'installable': True,
+    'application': False,
+}
